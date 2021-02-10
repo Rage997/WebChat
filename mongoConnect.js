9ce1cc12a33735d5f36dae73ec8df8bba724a855
Rage <pietrobianco@rage-mb.local>
2021-02-10 14:35:02 +0100

    Added some useful and basic mongoDB command

--- mongoConnect.js
+++ mongoConnect.js
@@ -5,3 +5,17 @@ mongoose.Promise  = require("bluebird");
 const  url  =  "mongodb://localhost:27017/chat";
 const  connect  =  mongoose.connect(url, { useNewUrlParser: true  });
 module.exports  =  connect;
+
+
+/*
+Some useful commands on mac
+
+1) Start, stop database
+brew services stop/start mongodb-community@4.4
+
+2) To delete database contents
+
+show dbs
+use <db_name>
+db .dropDatabase()
+*/
